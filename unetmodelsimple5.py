ff4fbd3223a22f0456f9a3b50637ab4ca1499595
christer-watson <64209508+christer-watson@users.noreply.github.com>
2022-02-25 21:08:34 -0500

    Add files via upload
    Initial submission. unetsimplefitting3.py does the training on whatever bubbles are designated in the file. Bands are Irac 3, Irac 4, WISE, MIPS, PACS and THOR (VLA 1060 MHz).
    unetmodelsimple5.py is the model. A CNN that is applied (same matrix) at three levels of resolution (full, cut by 1/2, cut by 1/4), then combined.

---
+++ unetmodelsimple5.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+#does a basic pool, conv, conv for the unet model
+
+def convmodel(inlayer, commonlayer1, commonlayer3, commonlayer7, pool_size=1):
+	layerX1 = tf.keras.layers.AveragePooling2D(pool_size = pool_size)(inlayer)
+	layerX2 = commonlayer1(layerX1)
+	layerX3 = tf.keras.layers.MaxPooling2D(pool_size=(4, 4))(layerX2)
+	layerX4 = commonlayer3(layerX3)
+	layerX5 = tf.keras.layers.MaxPooling2D(pool_size=(4, 4))(layerX4)
+	layerX6 = tf.keras.layers.UpSampling2D(size = (4,4))(layerX5)
+	layerX7 = tf.keras.layers.concatenate([layerX6, layerX4])
+	layerX8 = commonlayer7(layerX7)
+	layerX9 = tf.keras.layers.UpSampling2D(size=(4,4))(layerX8)
+	layerX10 = tf.keras.layers.concatenate([layerX9, layerX2])
+	layerX11 = tf.keras.layers.UpSampling2D(size=pool_size)(layerX10)
+	return(layerX11)
+
+def unetmodel(pretrained_weights = None,input_size = (1024,1024,6), initfilters=16):
+	inputs = tf.keras.layers.Input(shape=input_size)
+
+	commonlayer1 = tf.keras.layers.Conv2D(initfilters, 5, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal',name='commonlayer1')
+	commonlayer3 = tf.keras.layers.Conv2D(initfilters, 5, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal',name='commonlayer3')
+	commonlayer7 = tf.keras.layers.Conv2D(initfilters/2, 5, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal',name='commonlayer7')
+	
+	layerA = convmodel(inputs, commonlayer1, commonlayer3, commonlayer7, pool_size=1)
+	layerB = convmodel(inputs, commonlayer1, commonlayer3, commonlayer7, pool_size=2)
+	layerC = convmodel(inputs, commonlayer1, commonlayer3, commonlayer7, pool_size=4)
+	
+	
+	layer10 = tf.keras.layers.concatenate([layerA, layerB, layerC])
+	layer11 = tf.keras.layers.Conv2D(1,1,activation = 'sigmoid')(layer10)
+	model = tf.keras.models.Model(inputs = inputs, outputs = layer11)
+    
+#	model.summary()
+
+	if(pretrained_weights):
+		model.load_weights(pretrained_weights)
+
+	return model
+
